4c285fa94c7536b90dc63d099af4304db2cd8c26
ping19900226 <11987604+ping19900226@users.noreply.github.com>
2018-01-11 07:40:43 +0000

    增加部分操作，需要注意RequestHandler如何单利 HttpClient是否需要全局使用一个

--- src/main/java/com/yh/Launcher.java
+++ src/main/java/com/yh/Launcher.java
@@ -1,12 +1,19 @@
 package com.yh;
 
+import com.yh.request.CookieManager;
+import com.yh.request.YHContentHandlerFactory;
 import com.yh.view.WorkbenchView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.net.CookieHandler;
+import java.net.URLConnection;
+
 public class Launcher extends Application {
    @Override
    public void start(Stage primaryStage) throws Exception {
+      URLConnection.setContentHandlerFactory(new YHContentHandlerFactory());
+      CookieHandler.setDefault(com.yh.request.CookieStore.getCookieHander());
       new WorkbenchView().start(new Stage());
    }
 
